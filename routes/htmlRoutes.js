f7597b4e6d4468a1f5b1d537ac25bb55843de977
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-07 20:12:35 -0700

    added comments about debug logs

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,13 +3,13 @@ const router = require('express').Router();
 
 router.get('/', (req, res) =>
 {
-	console.log("Tried to send you index.html");
+	console.log("Tried to send you index.html"); // This never fires for reaons I don't understant.
 	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.get('/notes', (req, res) =>
 {
-	console.log("tried to send you notes.html");
+	console.log("Tried to send you notes.html"); // This fires just fine.
 	res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
