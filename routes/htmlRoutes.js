a8f0439e127c8be64e610a5e7e5359cda17b9d0d
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-07 20:09:42 -0700

    working on notes.html...

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,16 @@
 const path = require('path');
 const router = require('express').Router();
 
-router.get('notes', (req, res) =>
+router.get('/', (req, res) =>
 {
-	console.log("Tried to send you notes.html");
+	console.log("Tried to send you index.html");
 	res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
+router.get('notes', (req, res) =>
+{
+	console.log("tried to send you notes.html");
+	res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
 
 module.exports = router;
