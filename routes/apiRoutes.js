10ddc6addb40f4b0450e768e956e48256e1bb020
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-08 10:57:12 -0700

    works, I think

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,8 +5,10 @@ const fs = require('fs');
 router.get('/notes', (req, res) =>
 {
 	console.log("API bit to get notes...");
-	var notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8')) || {};
-	res.sendFile( notes );
+	//var notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8')) || {};
+	notes = fs.readFileSync('db/db.json', 'utf8');
+	console.log(notes);
+	res.send( notes );
 });
 
 module.exports = router;
