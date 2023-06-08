01863c283d9af954ed07b09e7b4df9fd03694f8d
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-08 11:14:55 -0700

    something wrong with making the db

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,9 +6,19 @@ router.get('/notes', (req, res) =>
 {
 	console.log("API bit to get notes...");
 	//var notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8')) || {};
-	notes = fs.readFileSync('db/db.json', 'utf8');
+	var notes = fs.readFileSync('db/db.json', 'utf8');
 	console.log(notes);
 	res.send( notes );
+});
+
+router.post('/notes', (req, res) =>
+{
+	var notes = fs.readFileSync('db/db.json', 'utf8');
+	const note = req.body;
+	console.log(note);
+	notes.push(note);
+	console.log(notes);
+	res.send({ title: "hello", text: "bob" });
 });
 
 module.exports = router;
