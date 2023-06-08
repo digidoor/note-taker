c6d6d8ff04a088ea872e8bd9f75fc0253a19e83a
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-08 10:22:12 -0700

    api get something wrong

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
+const fs = require('fs');
 
+router.get('/api/notes' (req, res) =>
+{
+	console.log("API bit to get notes...");
+	var notes = JSON.parse(readFile('db/db.json')) || {};
+	res.sendFile( notes );
+	var x;
+}
 
 module.exports = router;
