f108e11a17f24d03ee2d45f1cf5b09d7f8d4a0da
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-07 19:53:09 -0700

    first route in progress

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,2 +1,10 @@
 const path = require('path');
-router.get('notes', notes.html);
+const router = require('express').Router();
+
+router.get('notes', (req, res) =>
+{
+	console.log("Tried to send you notes.html");
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+
