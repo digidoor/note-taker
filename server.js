0aa1c9468707a5abaf95098365a35306d6e108fc
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-07 20:00:23 -0700

    apiRoutes filler and package-lock.json to gitign

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('.routes/htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,8 +9,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json())
 app.use( express.urlencoded({extended: true}) );
 app.use( express.static('public') );
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+//app.use('/api', apiRoutes);
+//app.use('/', htmlRoutes);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`) );
 
