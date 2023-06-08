450eefc1a76dfd843b75646f51a3722407e0137c
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-08 11:49:15 -0700

    unique IDs work now too

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const router = require('express').Router();
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) =>
 {
@@ -8,17 +9,21 @@ router.get('/notes', (req, res) =>
 	//var notes = JSON.parse(fs.readFileSync('db/db.json', 'utf8')) || {};
 	var notes = fs.readFileSync('db/db.json', 'utf8');
 	console.log(notes);
+	console.log( uuidv4() );
 	res.send( notes );
 });
 
 router.post('/notes', (req, res) =>
 {
 	var notes = JSON.parse( fs.readFileSync('db/db.json', 'utf8') );
-	const note = req.body;
+	var note = req.body;
 	console.log(note);
+	note.id = uuidv4();
+	console.log("note: ", note);
 	notes.push(note);
 	fs.writeFileSync('db/db.json', JSON.stringify(notes, null, '\t') );
 	console.log(notes);
+	console.log( uuidv4() );
 	res.send( JSON.stringify(note) );
 });
 
